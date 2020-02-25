742c0912eedbc13ceee5026d053687dced3186c3
Rafael dos Passos Fortes <rafael.pfortes@gmail.com>
2020-02-25 10:08:56 -0300

    Fixing phpDoc annotation

--- HelloWorld/Model/Post/DataProvider.php
+++ HelloWorld/Model/Post/DataProvider.php
@@ -29,9 +29,9 @@ class DataProvider extends AbstractDataProvider
 
     /**
      * DataProvider constructor.
-     * @param $name
-     * @param $primaryFieldName
-     * @param $requestFieldName
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
      * @param CollectionFactory $postCollectionFactory
      * @param StoreManagerInterface $storeManager
      * @param array $meta
